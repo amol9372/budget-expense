a262b91f67a7e12584c1c6418c76053b8c206bd9
Amol Singh <amol1.singh@scotiabank.com>
2022-11-12 10:55:22 -0500

    first commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -15,4 +15,4 @@ echo "Pulling image and starting container"
 
 docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 667631227859.dkr.ecr.us-east-1.amazonaws.com
 docker pull 667631227859.dkr.ecr.us-east-1.amazonaws.com/expenseapp
-docker run -it --env-file /home/ec2-user/docker/env_files/budgetapp.env -p 8082:8082 -d 667631227859.dkr.ecr.us-east-1.amazonaws.com/budgetapp
+docker run -it --env-file /home/ec2-user/docker/env_files/budgetapp.env -p 8083:8083 -d 667631227859.dkr.ecr.us-east-1.amazonaws.com/expenseapp
