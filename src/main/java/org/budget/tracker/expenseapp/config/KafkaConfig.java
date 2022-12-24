5407a8c673c1a99b970676b06991b10510125d82
Amol Singh <amol1.singh@scotiabank.com>
2022-12-24 11:38:32 -0500

    confluent kafka

--- src/main/java/org/budget/tracker/expenseapp/config/KafkaConfig.java
+++ src/main/java/org/budget/tracker/expenseapp/config/KafkaConfig.java
@@ -1,7 +1,9 @@
 package org.budget.tracker.expenseapp.config;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +18,10 @@ public class KafkaConfig {
 
     @Value("${kafka.bootstrap}")
     private String bootstrapServers;
+    @Value("${kafka.user}")
+    private String user;
+    @Value("${kafka.password}")
+    private String password;
 
     @Bean
     public KafkaProducer<String, Object> producer() {
@@ -24,6 +30,17 @@ public class KafkaConfig {
         props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaJsonSerializer.class.getName());
+        props.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+        props.put(
+                SaslConfigs.SASL_JAAS_CONFIG,
+                "org.apache.kafka.common.security.plain.PlainLoginModule required "
+                        + "username='"
+                        + user
+                        + "'"
+                        + " password='"
+                        + password
+                        + "';");
         return new KafkaProducer<>(props);
     }
 
